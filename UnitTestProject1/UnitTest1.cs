54e22bc228ee9f5e28708b684d82b32c23a5a0a8
Erwin Martin Gutbrod <egutbrod@makingsense.com>
2022-08-24 10:52:07 -0300

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using WindowsFormsApp1;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var asd = new Form1();
+
+            Assert.NotNull(asd);
+        }
+    }
+}
